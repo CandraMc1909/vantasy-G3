16ea31db504a995b22f65b824bb5a15d0a813733
CandraMc1909 <candragg303@gmail.com>
2026-07-30 07:43:03 +0700

    date Update

--- commands/menu.js
+++ commands/menu.js
@@ -4,7 +4,7 @@ module.exports = {
     from,
     msg
   ) => {
-    const menu = ` *˚₊·˚₊· ͟͟͞͞➳❥* ✧𝕮𝖔𝖑𝖚𝖒𝖇𝖎𝖓𝖆✧\n*☆═━┈◈ ╰ v26.03.25 ╯ ◈┈━═☆* \n*│* [ (.),(/),(!),(-),(<),(>),(🥩) ]\n*╰ ㊂▸▸ _INFORMASI MENU_ ◂◂*\n*│* ┊\n*│* ┊▸ ✦ _/comand_help_\n*│* ┊▸ ✦ _/bio_\n*│* ┊▸ ✦ _/donet_\n*│* ┊▸ ✦ _/login_\n*│* ┊▸ ✦ /server_info\n*│* ╰∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙ ∙ ∙ ∙ ∙ \n*│*\n*╰ ㊂ ▸▸✧･ﾟ: * 𝕮𝖔𝖑𝖚𝖒𝖇𝖎𝖓𝖆 *:･ﾟ✧◂◂*\n\n GC [https://chat.whatsapp.com/BTSwrfGbvIK9IX5CeuQlkE?s=cl&p=a&ilr=4]`
+    const menu = ` *˚₊·˚₊· ͟͟͞͞➳❥* ✧𝕮𝖔𝖑𝖚𝖒𝖇𝖎𝖓𝖆✧\n*☆═━┈◈ ╰ v26.03.25 ╯ ◈┈━═☆* \n*│*\n*╰ ㊂▸▸ _INFORMASI MENU_ ◂◂*\n*│* ┊\n*│* ┊▸ ✦ _/comand_help_\n*│* ┊▸ ✦ _/bio_\n*│* ┊▸ ✦ _/donet_\n*│* ┊▸ ✦ _/login_\n*│* ┊▸ ✦ _/server_info_\n*│* ╰∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙ ∙ ∙ ∙ ∙ \n*│*\n*╰ ㊂ ▸▸✧･ﾟ: * 𝕮𝖔𝖑𝖚𝖒𝖇𝖎𝖓𝖆 *:･ﾟ✧◂◂*\n\n GC [https://chat.whatsapp.com/BTSwrfGbvIK9IX5CeuQlkE?s=cl&p=a&ilr=4]`
     await sock.sendMessage(
       from,
       {
